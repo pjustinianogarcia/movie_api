b23b558c64f46834408597516154c813c79a5a10
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-04-15 19:42:15 -0400

    Added movie database to index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+// import express module to file
+const express = require('express');
+// variable to route your HTTP requests and responses
+const app = express();
+
+let topMovies = [
+    { title: 'Means Girls', year: 2004 },
+    { title: 'What a Girl Wants', year: 2003 },
+    { title: 'Clueless', year: 1995 },
+    { title: 'The 10 Things I Hate About You', year: 1999 },
+    { title: 'Heathers', year: 1988 },
+    { title: 'Donnie Darko', year: 2001 },
+    { title: 'Cruel Intentions ', year: 1999 },
+    { title: 'Jennifers Body', year: 2009 },
+    { title: 'John Tucker Must Die', year: 2006 },
+    { title: 'Dazed and Confused', year: 1993 }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie list!');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
