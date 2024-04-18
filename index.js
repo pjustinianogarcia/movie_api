d15eb8ce209cabfcf88791b8af6025493abb05fc
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-04-17 23:35:33 -0400

    Added error handling function

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // import express module to file
 const express = require('express');
+//import morgan
+const morgan = require('morgan');
 // variable to route your HTTP requests and responses
 const app = express();
+
+app.use(morgan('common'));
 
 let topMovies = [
     { title: 'Means Girls', year: 2004 },
@@ -19,19 +23,25 @@ let topMovies = [
 // static function
 app.use(express.static('public'));
 
+
 // GET requests
 app.get('/', (req, res) => {
   res.send('Welcome to my movie list!');
 });
 
-app.get('/documentation', (req, res) => {                  
-  res.sendFile('public/documentation.html', { root: __dirname });
-});
+// app.get('/documentation', (req, res) => {                  
+//   res.sendFile('public/documentation.html', { root: __dirname });
+// });
 
 app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
+//error handling function
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
 
 // listen for requests
 app.listen(8080, () => {
