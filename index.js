e9fa825b0fd2f89b0bbec913ec539c800b23eae5
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-05-05 19:10:42 -0400

    added express-validator

--- index.js
+++ index.js
@@ -118,7 +118,21 @@ app.get("/directors/:Name", passport.authenticate('jwt', { session: false }), as
 
 
 //add user
-app.post("/users", passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.post("/users", 
+//passport.authenticate('jwt', { session: false }), async (req, res) => {
+    [
+        check('Username', 'Username is required').isLength({min: 5}),
+        check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+        check('Password', 'Password is required').not().isEmpty(),
+        check('Email', 'Email does not appear to be valid').isEmail()
+      ], async (req, res) => {
+    
+      // check the validation object for errors
+        let errors = validationResult(req);
+    
+        if (!errors.isEmpty()) {
+          return res.status(422).json({ errors: errors.array() });
+        }
     let hashedPassword = Users.hashPassword(req.body.Password);
     await Users.findOne({ Username: req.body.Username })
         .then((user) => {
