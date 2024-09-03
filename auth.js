f902e3db82147df445335ea89ab901c796e6bc32
pjustinianogarcia <158845576+pjustinianogarcia@users.noreply.github.com>
2024-09-02 22:46:41 -0400

    Generated  JSdocs

--- auth.js
+++ auth.js
@@ -1,41 +1,37 @@
-/**
- * @module auth
- * @description This module handles user authentication using JWT (JSON Web Token).
- */
-const jwtSecret = 'your_jwt_secret'; 
+// This module handles user authentication using JWT (JSON Web Token).
+const jwtSecret = "your_jwt_secret";
 //import modules
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
-require('./passport');
+const jwt = require("jsonwebtoken"),
+  passport = require("passport");
+require("./passport");
 
 /**
  * Generates a JWT token for a user.
- * 
+ *
  * @function generateJWTToken
  * @param {Object} user - The user object for which the token is generated.
  * @returns {string} The generated JWT token.
  */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.Username, 
-    expiresIn: '7d',
-    algorithm: 'HS256' 
+    subject: user.Username,
+    expiresIn: "7d",
+    algorithm: "HS256",
   });
-}
-
+};
 
 /**
  * POST login route. Authenticates a user and generates a JWT token upon successful login.
- * 
+ *
  * @param {Object} router - The Express router object.
  */
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
+  router.post("/login", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: 'Something is not right',
-          user: user
+          message: "Something is not right",
+          user: user,
         });
       }
       req.login(user, { session: false }, (error) => {
@@ -47,4 +43,4 @@ module.exports = (router) => {
       });
     })(req, res);
   });
-}
+};
--- passport.js
+++ passport.js
@@ -1,8 +1,8 @@
 //import modules
-const passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+const passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy,
+  Models = require("./models.js"),
+  passportJWT = require("passport-jwt");
 
 let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
@@ -10,7 +10,7 @@ let Users = Models.User,
 
 /**
  * Configures Passport to use the LocalStrategy for username and password authentication.
- * 
+ *
  * @function
  * @param {string} username - The username of the user attempting to log in.
  * @param {string} password - The password of the user attempting to log in.
@@ -19,49 +19,54 @@ let Users = Models.User,
 passport.use(
   new LocalStrategy(
     {
-      usernameField: 'Username',
-      passwordField: 'Password',
+      usernameField: "Username",
+      passwordField: "Password",
     },
     async (username, password, callback) => {
       console.log(`${username} ${password}`);
-      
+
       await Users.findOne({ Username: username })
-      .then((user) => {
-        if (!user.validatePassword(password)) {
-          console.log('incorrect username');
-          return callback(null, false, {
-            message: 'Incorrect username or password.',
-          });
-        }
-        console.log('finished');
-        return callback(null, user);
-      })
-      .catch((error) => {
-        if (error) {
-          console.log(error);
-          return callback(error);
-        }
-      })
+        .then((user) => {
+          if (!user.validatePassword(password)) {
+            console.log("incorrect username");
+            return callback(null, false, {
+              message: "Incorrect username or password.",
+            });
+          }
+          console.log("finished");
+          return callback(null, user);
+        })
+        .catch((error) => {
+          if (error) {
+            console.log(error);
+            return callback(error);
+          }
+        });
     }
   )
 );
 
 /**
  * Configures Passport to use JWTStrategy for token-based authentication.
- * 
+ *
  * @function
  * @param {Object} jwtPayload - The payload extracted from the JWT.
  * @param {Function} callback - The callback function to handle the result of the authentication.
  */
-passport.use(new JWTStrategy({
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
-}, async (jwtPayload, callback) => {
-  return await Users.findById(jwtPayload._id)
-    .then((user) => {
-      return callback(null, user);
-    })
-    .catch((error) => {
-      return callback(error)
-    });
-}));
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "your_jwt_secret",
+    },
+    async (jwtPayload, callback) => {
+      return await Users.findById(jwtPayload._id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((error) => {
+          return callback(error);
+        });
+    }
+  )
+);
